b494206184d66e88d1933f7829f813d4bfb1af9d
ArtuTerra <arthur.terra@hotmail.com>
2023-06-19 20:22:14 -0300

    Added local storage to tasks!

--- script.js
+++ script.js
@@ -1,43 +1,90 @@
 let botaoAdicionarAfazer = document.getElementById("adicionarAfazer");
-
 let conteinerAfazeres = document.getElementById("conteinerAfazeres");
-
 let input = document.getElementById("input");
+let afazeres = [];
 
-botaoAdicionarAfazer.addEventListener("click", function () {
-  var paragrafo = document.createElement("p");
+
+function loadFromStorage() {
+  let afazeresString = localStorage.getItem("afazeres");
+  if (afazeresString) {
+    let tarefas = JSON.parse(afazeresString);
+    for (let tarefa of tarefas) {
+      let paragrafo = document.createElement("p");
+      paragrafo.classList.add("style-paragrafo");
+      paragrafo.innerText = tarefa.texto;
+      if (tarefa.cruzado) {
+        paragrafo.style.textDecoration = "line-through";
+      }
+      conteinerAfazeres.appendChild(paragrafo);
+      afazeres.push(tarefa);
+      paragrafo.addEventListener("click", function () {
+        if (paragrafo.style.textDecoration === "line-through") {
+          paragrafo.style.textDecoration = "none";
+          afazeres.find(
+            (el) => el.texto === paragrafo.innerText
+          ).cruzado = false;
+        } else {
+          paragrafo.style.textDecoration = "line-through";
+          afazeres.find(
+            (el) => el.texto === paragrafo.innerText
+          ).cruzado = true;
+        }
+        salvar();
+      });
+      paragrafo.addEventListener("dblclick", function () {
+        conteinerAfazeres.removeChild(paragrafo);
+        afazeres.splice(
+          afazeres.findIndex((el) => el.texto === paragrafo.innerText),
+          1
+        );
+        salvar();
+      });
+    }
+  }
+}
+
+loadFromStorage();
+
+botaoAdicionarAfazer.addEventListener("click", addTarefa);
+
+input.addEventListener("keydown", function (evento) {
+  if (evento.key === "Enter") {
+    addTarefa();
+  }
+});
+
+function addTarefa() {
+  if (input.value === "") {
+    return console.error("No text!");
+  }
+  let paragrafo = document.createElement("p");
   paragrafo.classList.add("style-paragrafo");
   paragrafo.innerText = input.value;
   conteinerAfazeres.appendChild(paragrafo);
+  afazeres.push({ texto: input.value, cruzado: false });
+  salvar();
   input.value = "";
   paragrafo.addEventListener("click", function () {
     if (paragrafo.style.textDecoration === "line-through") {
       paragrafo.style.textDecoration = "none";
+      afazeres.find((el) => el.texto === paragrafo.innerText).cruzado = false;
     } else {
       paragrafo.style.textDecoration = "line-through";
+      afazeres.find((el) => el.texto === paragrafo.innerText).cruzado = true;
     }
+    salvar();
   });
   paragrafo.addEventListener("dblclick", function () {
     conteinerAfazeres.removeChild(paragrafo);
+    afazeres.find((el) => el.texto === paragrafo.innerText).cruzado = true;
+    afazeres.splice(
+      afazeres.findIndex((el) => el.texto === paragrafo.innerText),
+      1
+    );
+    salvar();
   });
-});
+}
 
-input.addEventListener("keydown", function (evento) {
-  if (evento.key === "Enter") {
-    var paragrafo = document.createElement("p");
-    paragrafo.classList.add("style-paragrafo");
-    paragrafo.innerText = input.value;
-    conteinerAfazeres.appendChild(paragrafo);
-    input.value = "";
-    paragrafo.addEventListener("click", function () {
-      if (paragrafo.style.textDecoration === "line-through") {
-        paragrafo.style.textDecoration = "none";
-      } else {
-        paragrafo.style.textDecoration = "line-through";
-      }
-    });
-    paragrafo.addEventListener("dblclick", function () {
-      conteinerAfazeres.removeChild(paragrafo);
-    });
-  }
-});
+function salvar() {
+  localStorage.setItem("afazeres", JSON.stringify(afazeres));
+}
